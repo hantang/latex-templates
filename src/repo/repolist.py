8f7f1838ce41bde26f7080a22b63075f42ae134f
hantang <34468720+hantang@users.noreply.github.com>
2024-02-17 15:16:17 +0800

    Add src

---
+++ src/repo/repolist.py
@@ -0,0 +1,447 @@
+from datetime import datetime
+from pathlib import Path
+import json
+import logging
+import pandas as pd
+import re
+
+from .config import NAME_MAP, REPO_STRUCTS, COLUMNS2, COLUMNS
+from .utils import strip_url, strip, get_pinyin_key
+from .repostats import RepoStats
+
+
+TAG_ARCHIVED = "📦"  # :package: 已归档
+TAG_OBSOLETE = "🔒️"  # :lock: 长久未更新
+TAG_REMOVED = "🗑️"  # :wastebasket:
+
+
+def _get_status_obsolete(repo_info, date_now=None, date_gap=1461):
+    date_update = max([repo_info[key] for key in ["pushed_at", "created_at"]])
+    date_update = datetime.strptime(date_update, "%Y-%m-%dT%H:%M:%SZ")
+    if not date_now:
+        date_now = datetime.utcnow()
+    if (date_now - date_update).days > date_gap:
+        return TAG_OBSOLETE
+    return None
+
+
+def _get_status_archive(repo_info):
+    return TAG_OBSOLETE if repo_info["archived"] else None
+
+
+def _get_score(stargazers, fork):
+    return stargazers + fork * 2
+
+
+def _get_badges(repo, pushed_at, created_at, stargazers, forks, license):
+    """_summary_
+    🔖`2011-09-12` ![stargazers_count=4122](https://flat.badgen.net/github/stars/tuna/thuthesis) ![forks_count=1040](https://flat.badgen.net/github/forks/tuna/thuthesis) ![pushed_at=2023-11-07](https://flat.badgen.net/github/last-commit/tuna/thuthesis) ![license=LPPL-1.3c](https://flat.badgen.net/github/license/tuna/thuthesis)
+    Args:
+        repo_info (_type_): _description_
+    """
+    REPO_STATS_URL = "https://flat.badgen.net/github"
+    REPO_STATS_KEYS = ["stars", "forks", "last-commit", "license"]
+    REPO_STATS_VALS = [stargazers, forks, pushed_at, license]
+
+    badges = []
+    for key, val in zip(REPO_STATS_KEYS, REPO_STATS_VALS):
+        badges.append(f"![{key}={val}]({REPO_STATS_URL}/{key}/{repo})")
+    badges = " ".join(badges)
+    return f"🎉`{created_at}` {badges}".rstrip()
+
+def _get_description(description):
+    return strip(re.sub(r"\\(latex)\\?", r"\1", description, flags=re.IGNORECASE) if description else description)
+
+def _get_desc(archived, obslete, description):
+    tags = "/".join([v for v in [archived, obslete] if v])
+    desc = " ".join([tags, (f"`{description}`" if description else "")])
+    return desc.strip()
+
+
+def _get_title(zh_name, en_name, repository):
+    return f"- **{zh_name}**（{en_name}）: {repository}" if zh_name else repository
+
+
+
+def to_df(entries, repo_dict, date_now=None):
+    out = []
+    for entry in entries:
+        out_entries = []
+        archived_cnt = 0
+        obsolete_cnt = 0
+        for link in entry["links"]:
+            repo_info = repo_dict[link]
+            archived = _get_status_archive(repo_info)
+            obsolete = _get_status_obsolete(repo_info, date_now)
+            if archived:
+                archived_cnt += 1
+            if obsolete:
+                obsolete_cnt += 1
+            full_name, url = repo_info["full_name"], repo_info["html_url"]
+            result = {
+                "zh": entry.get("zh"),
+                "en": entry.get("en"),
+                "count": len(entry["links"]),
+                "repository": f"[{full_name}]({url})",
+                "archived": archived,
+                "obsolete": obsolete,
+                "stargazers": repo_info["stargazers_count"],
+                "forks": repo_info["forks_count"],
+                # "subscribers":repo_info["subscribers_count"],
+                "pushed_at": repo_info["pushed_at"].split("T")[0],
+                "created_at": repo_info["pushed_at"].split("T")[0],
+                # "updated_at": repo_info["updated_at"].split("T")[0],
+                "license": repo_info["license_name"],
+                "description": _get_description(repo_info["description"]),
+            }
+            # more
+            result["score"] = _get_score(result["stargazers"], result["forks"])
+            result["badges"] = _get_badges(
+                repo_info["full_name"],
+                result["pushed_at"],
+                result["created_at"],
+                result["stargazers"],
+                result["forks"],
+                result["license"],
+            )
+            result["desc"] = _get_desc(archived, obsolete, result["description"])
+            result["title"] = _get_title(
+                result["zh"], result["en"], result["repository"]
+            )
+            out_entries.append(result)
+        for res in out_entries:
+            res["count"] = "{}/{}/{}".format(res["count"], archived_cnt, obsolete_cnt)
+        out.extend(out_entries)
+    if len(out) == 0:
+        df = pd.DataFrame(data=None, columns=COLUMNS)
+    else:
+        df = pd.DataFrame(out)
+    df = df.fillna(" ").astype(str)
+    df = df.reset_index()
+    return df
+
+
+def to_list(df_entry, top=10, order=True):
+    key = "score"
+    df_entry[key] = df_entry[key].astype("int")
+    df_entry2 = df_entry[df_entry[key] > 0]
+    if order:
+        df_entry2 = df_entry2.sort_values(key, ascending=False)
+    if top > 0:
+        df_entry2 = df_entry2.head(top)
+    texts = []
+    blank = " " * 2
+    entries = df_entry2.to_dict("records")
+    for e in entries:
+        texts.append(e["title"])
+        texts.extend(["{}- {}".format(blank, v) for v in [e["desc"], e["badges"]] if v])
+    if len(df_entry2) < len(df_entry):
+        texts.append("- ...")
+    return texts
+
+
+class RepoList:
+    def __init__(self, datadir, config, is_thesis=True) -> None:
+        self.datadir = datadir
+        self.config = config
+        self.is_theis = is_thesis
+
+        self.key_zh = "zh"
+        self.key_en = "en"
+        self.key_links = "links"
+
+        self.name = self._init_file()
+        self.file = Path(self.datadir, self.name)
+        self.groups = self._init_groups()
+        self.data = self._readfile()
+
+    def _init_file(self):
+        config = self.config
+        return config["name"] + ".json"
+
+    def _init_groups(self):
+        config = self.config
+        if "categories" in config:
+            categories = config["categories"]
+            return [[group, categories] for group in config["groups"]]
+        else:
+            return config["groups"]
+
+    def _readfile(self):
+        file = self.file
+        if not file.exists():
+            logging.warning("Error file `{file}` does not exist")
+            return None
+
+        logging.info(f"Read file `{file}`")
+        with open(file) as f:
+            return json.load(f)
+
+    def get_links(self):
+        links = []
+        repo_data = self.data
+        if not repo_data:
+            return links
+        for _, group_values in repo_data.items():
+            for _, cate_values in group_values.items():
+                for entry in cate_values:
+                    links.extend([strip_url(link) for link in entry[self.key_links]])
+        return links
+
+    def update_data(self, stats, dup_links):
+        file = self.file
+        repo_data = self.data
+        groups = self.groups
+
+        error_links = stats.get_error_repos()
+        renamed_links_dict = stats.get_renamed_repos()
+
+        logging.info(f"Process file = {file}")
+        out_repo_data = {}
+        for group, categories in groups:
+            for cate in categories:
+                entries = []
+                for entry in repo_data[group][cate]:
+                    links2 = []
+                    for link in entry[self.key_links]:
+                        link = strip_url(link)
+                        if link in dup_links:
+                            logging.info(f"Duplicated repo: {link}")
+                            continue
+                        if link in renamed_links_dict:
+                            link2 = renamed_links_dict[link]
+                            logging.info(f"Renamed repo: {link} -> {link2}")
+                            link = link2
+                        if link in error_links:
+                            logging.info(f"Error repo: {link}")
+                            continue
+
+                        dup_links.append(link)
+                        links2.append(link)
+
+                    entry2 = {}
+                    if self.key_zh in entry:
+                        entry2[self.key_zh] = entry[self.key_zh]
+                        entry2[self.key_en] = entry[self.key_en]
+                    entry2[self.key_links] = sorted(links2)
+                    entries.append(entry2)
+
+                entries = sorted(entries, key=get_pinyin_key)  # TODO
+                if group not in out_repo_data:
+                    out_repo_data[group] = {}
+                out_repo_data[group][cate] = entries
+
+        self.data = out_repo_data
+        return dup_links
+
+    def save(self):
+        file = self.file
+        out_repo_data = self.data
+        logging.info(f"Rebuild file = {file}")
+        with open(file, "w") as fw:
+            json.dump(out_repo_data, fw, indent=2, ensure_ascii=False)
+
+    def _to_df(self, stats: RepoStats):
+        repo_data = self.data
+        groups = self.groups
+        date_now = stats.now
+        repo_dict = stats.get_repos_dict()
+
+        df_list = []
+        for group, categories in groups:
+            for cate in categories:
+                entries = repo_data[group][cate]
+                entries = repo_data[group][cate]
+                df = to_df(entries, repo_dict, date_now)
+                df_list.append((group, cate, df))
+        return df_list
+
+    def save_wiki(self, docdir, csvdir, stats):
+        csvdir = Path(csvdir)
+        docdir = Path(docdir)
+        if not csvdir.exists():
+            logging.info(f"Make dir {csvdir}")
+            csvdir.mkdir(parents=True)
+        if not docdir.exists():
+            logging.info(f"Make dir {docdir}")
+            docdir.mkdir(parents=True)
+
+        columns = COLUMNS2
+        if not self.is_theis:
+            columns = columns[2:]
+        df_list = self._to_df(stats)
+        md_texts = {}
+        logging.info("Save to wiki csv")
+        for group, cate, df in df_list:
+            group_name = NAME_MAP[group]
+            cate_name = NAME_MAP[cate]
+            savefile = Path(csvdir, f"{group_name}-{cate_name}.csv")
+            logging.info(f"Save to csv to {savefile}")
+            df[["index"] + columns].to_csv(savefile, index=False)
+            if group not in md_texts:
+                md_texts[group] = [f"# {group}"]
+            md_texts[group].extend(
+                [
+                    f"## {cate}",
+                    f'{{{{ read_csv("../{csvdir.name}/{savefile.name}") }}}}',
+                ]
+            )
+
+        logging.info("Save to wiki docs")
+        for group, texts in md_texts.items():
+            group_name = NAME_MAP[group]
+            savefile = Path(docdir, f"{group_name}.md")
+            logging.info(f"Save to markdown to {savefile}")
+            with open(savefile, "w") as fw:
+                fw.write("\n\n".join(texts).strip() + "\n")
+
+    def get_thesis_list(self, stats, zh=True, latex=True, top=10):
+        topic1 = "学位论文模板"
+        topic2 = "LaTeX"
+        groups = self.groups[:2] if zh else self.groups[2:4]
+        df_list = self._to_df(stats)
+
+        keywords = []
+        for group, categories in groups:
+            for cate in categories:
+                if topic1 not in cate:
+                    continue
+                if latex and topic2 in cate:
+                    keywords.append((group, cate))
+                if not latex and topic2 not in cate:
+                    keywords.append((group, cate))
+        df = [df for group, cate, df in df_list if (group, cate) in keywords]
+        df = pd.concat(df)
+        out = to_list(df, top=top)
+
+        toc = []
+        texts = []
+        prefix = "" if latex else "非"
+        if out:
+            header = f"### {topic1}（{prefix}{topic2}）"
+            toc.append(header)
+            texts = ["", header, ""] + out
+        return toc, texts
+
+    def get_other_list(self, stats, top=5):
+        details = ["<details>\n<summary>点击展开</summary>", "</details>"]
+        groups = self.groups
+        df_list = self._to_df(stats)
+
+        keywords = [group for group, _ in groups]
+        toc = []
+        texts = []
+        for group, cate, df in df_list:
+            if group not in keywords:
+                continue
+            out = to_list(df, top=top)
+            if out:
+                header = f"### {cate}"
+                toc.append(header)
+                texts.extend(["", header, "", details[0], ""] + out + ["", details[1]])
+        return toc, texts
+
+
+class RepoLists:
+    def __init__(self, datadir, docdir, csvdir, readme_file) -> None:
+        self.datadir = datadir
+        self.docdir = docdir
+        self.csvdir = csvdir
+        self.readme_file = readme_file
+
+        self.thesis_repo_list = RepoList(datadir, REPO_STRUCTS[0], is_thesis=True)
+        self.other_repo_list = RepoList(datadir, REPO_STRUCTS[1], is_thesis=False)
+
+    def get_links(self):
+        links1 = self.thesis_repo_list.get_links()
+        links2 = self.other_repo_list.get_links()
+
+        links = sorted(set(links1 + links2))
+        return links
+
+    def rebulid(self, stats):
+        dup_links = []
+        dup_links = self.thesis_repo_list.update_data(stats, dup_links)
+        dup_links = self.other_repo_list.update_data(stats, dup_links)
+
+        self.thesis_repo_list.save()
+        self.other_repo_list.save()
+
+    def update_wiki(self, stats):
+        self.thesis_repo_list.save_wiki(self.docdir, self.csvdir, stats)
+        self.other_repo_list.save_wiki(self.docdir, self.csvdir, stats)
+
+    def update_readme(self, stats):
+        toc = ["# Beyond LaTeX Templates", "## 说明"]
+        head_list = [
+            "## 最受欢迎的LaTeX学位论文模板（中文）",
+            "## 最受欢迎LaTeX学位论文模板（其他）",
+            "## 更多模板资源",
+        ]
+        readmefile = self.readme_file
+        s = datetime.utcnow().strftime("%Y-%m-%d")
+        if not Path(readmefile).exists():
+            logging.warning(f"Readme file {readmefile} does not exist")
+            return
+
+        # list
+        toc0a, text0a = self.thesis_repo_list.get_thesis_list(
+            stats, True, latex=True, top=25
+        )
+        toc0b, text0b = self.thesis_repo_list.get_thesis_list(
+            stats, True, latex=False, top=15
+        )
+
+        toc1a, text1a = self.thesis_repo_list.get_thesis_list(
+            stats, False, latex=True, top=15
+        )
+        toc1b, text1b = self.thesis_repo_list.get_thesis_list(
+            stats, False, latex=False, top=10
+        )
+
+        toc2, text2 = self.other_repo_list.get_other_list(stats, top=5)
+
+        # toc
+        toc += [head_list[0]] + toc0a + toc0b
+        toc += [head_list[1]] + toc1a + toc1b
+        toc += [head_list[2]] + toc2
+        toc_list = []
+        for line in toc:
+            hashtag, title = re.findall(r"(#+) (.+)", line)[0]
+            hash_len = (len(hashtag) - 1) * 2
+            title2 = title.lower().replace(" ", "-")
+            toc_list.append("{}- [{}](#{})".format(" " * hash_len, title, title2))
+
+        # readme
+        sections = ["lastmod", "toc", "toplist0", "toplist1", "toplist2"]
+        paragraphs = [
+            f"最近更新：*{s}*",
+            "\n".join(toc_list),
+            "\n".join([head_list[0]] + text0a + text0b),
+            "\n".join([head_list[1]] + text1a + text1b),
+            "\n".join([head_list[2]] + text2),
+        ]
+
+        logging.info(f"Read {readmefile}")
+        with open(readmefile) as f:
+            text = f.read()
+
+        for sec, para in zip(sections, paragraphs):
+            logging.info(f"sec = {sec}")
+            begin, end = f"<!-- {sec} -->", f"<!-- end-{sec} -->"
+            pattern = rf"{begin}(.|\n)+{end}"
+            to = "\n\n".join([begin, para, end])
+            text = re.sub(pattern, lambda m: to, text)
+
+        logging.info(f"Save to {readmefile}")
+        with open(readmefile, "w") as fw:
+            fw.write(text)
+
+        wikifile = Path(self.docdir, "index.md")
+        more = [
+            "# Beyond LaTeX Templates",
+            "**Welcome to awesome latex-templates wiki!**",
+        ] + paragraphs[2:4]
+        logging.info(f"Save to wiki: {wikifile}")
+        with open(wikifile, "w") as fw:
+            fw.write("\n\n".join(more) + "\n")
---
+++ src/repo/repostats.py
@@ -0,0 +1,187 @@
+from datetime import datetime
+from pathlib import Path
+import json
+import logging
+
+from .config import REPO_KEY_URL
+from .utils import random_sleep
+from .repo import Repo
+
+
+class RepoStats:
+    def __init__(self, name, links) -> None:
+        self.filename = name
+        self.links = links
+        self.repos = []
+        self.error_repos = []
+        self.renamed_repos = {}
+        self.temp_file = "temp/temp.json"
+        self.key_repos = "repositories"
+        self.key_error = "error"
+        self.key_renamed = "renamed"
+        self.key_renamed_url = "url"
+        self.key_renamed_alias = "alias"
+        self.key_time = "update_time"
+        self._now = datetime.utcnow()
+        self._readfile()
+
+    def _readfile(self):
+        file = Path(self.filename)
+        if file.exists():
+            logging.info(f"Read and init file = {file}")
+            with open(file) as f:
+                data = json.load(f)
+            # self.repos = data[self.key_repos]
+            self.error_repos = data[self.key_error]
+            self.renamed_repos = data[self.key_renamed]
+
+    def query(self, tokens):
+        links = self.links
+        temp_file = Path(self.temp_file)
+        key_repos = self.key_repos
+        key_error = self.key_error
+
+        logging.info(f"Query repo api: repo_list = {len(links)}")
+        temp_data = {key_repos: [], key_error: []}
+        if temp_file.exists():
+            logging.info(f"Read temp file={temp_file}")
+            with open(temp_file) as f:
+                temp_data = json.load(f)
+        else:
+            if not temp_file.parent.exists():
+                logging.info(f"Create temp file dir {temp_file.parent}")
+                temp_file.parent.mkdir(parents=True)
+
+        queried_links = [e[0] for e in temp_data[key_repos]] + temp_data[key_error]
+        logging.info(f"Query repo api: queried = {len(queried_links)}")
+
+        total = len(links)
+        for idx, repo_url in enumerate(links):
+            if repo_url in queried_links:
+                continue
+            if (idx + 1) % 5 == 0 or idx == 0:
+                logging.info(f"Query repo api: round = {idx+1:03d}/{total:03d}")
+
+            random_sleep(idx + 1, step=17, second=17)
+            repo = Repo(repo_url)
+            repo.query(tokens, sleep=idx % 2 == 0)
+
+            if repo.repo_stats:
+                temp_data[key_repos].append([repo_url, repo.repo_stats])
+            elif repo.error:
+                temp_data[key_error].append(repo_url)
+            with open(temp_file, "w") as f:
+                json.dump(temp_data, f, indent=2, ensure_ascii=False)
+
+            if repo.is_403:
+                logging.info("Query break, status code is 403")
+                break
+
+        with open(temp_file, "w") as f:
+            json.dump(temp_data, f, indent=2, ensure_ascii=False)
+        queried_cnt = len(temp_data[key_repos])
+        error_cnt = len(temp_data[key_error])
+        logging.info(
+            f"Query repo api: queried/error/total = {queried_cnt}/{error_cnt}/{total}"
+        )
+
+        temp_repo_data = {
+            key_repos: [Repo(url, stats) for url, stats in temp_data[key_repos]],
+            key_error: temp_data[key_error],
+        }
+        return temp_repo_data
+
+    def process(self, token, retry=5):
+        # temp_file = self.temp_file
+        key_repos = self.key_repos
+        key_error = self.key_error
+        # key_renamed = self.key_renamed
+        key_renamed_url = self.key_renamed_url
+        key_renamed_alias = self.key_renamed_alias
+
+        for i in range(retry):
+            logging.info(f">> Retry ... {i+1}/{retry}")
+            temp_repo_data = self.query(token)
+            if len(temp_repo_data[key_repos]) == len(self.links):
+                break
+            else:
+                random_sleep(1, 1, 180)
+        if len(temp_repo_data[key_repos]) != len(self.links):
+            logging.warning("Error repo links count")
+            return
+        logging.info(f"Process repo: temp data = {len(temp_repo_data[key_repos])}")
+
+        renamed_repos = self.renamed_repos
+        logging.info(f"Before process repo: renamed = {len(renamed_repos)}")
+
+        error_repos = self.error_repos
+        logging.info(f"Before process repo: error = {len(error_repos)}")
+
+        repos = {}
+        for repo in temp_repo_data[key_repos]:
+            repo_id = repo.get_id()
+            raw_repo_url = repo.get_url_raw()
+            new_repo_url = repo.get_url()
+            repos[repo_id] = repo.get_stats()
+
+            if new_repo_url != raw_repo_url:
+                if repo_id in renamed_repos:
+                    renamed_repos[repo_id][key_renamed_alias].append(raw_repo_url)
+                else:
+                    renamed_repos[repo_id] = {
+                        key_renamed_url: new_repo_url,
+                        key_renamed_alias: [new_repo_url, raw_repo_url],
+                    }
+        renamed_repos = {
+            k: {
+                key_renamed_url: v[key_renamed_url],
+                key_renamed_alias: sorted(set(v[key_renamed_alias])),
+            }
+            for k, v in renamed_repos.items()
+        }
+        error_repos = sorted(set(error_repos + temp_repo_data[key_error]))
+
+        self.repos = [repos[repo_id] for repo_id in sorted(repos.keys())]
+        self.error_repos = error_repos
+        self.renamed_repos = renamed_repos
+        logging.info(
+            f"Processed repo: total/error/renamed repos = {len(self.repos)}/{len(self.error_repos)}/{len(self.renamed_repos)}"
+        )
+
+    def output(self):
+        return {
+            self.key_time: self._now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            self.key_repos: self.repos,
+            self.key_error: self.error_repos,
+            self.key_renamed: self.renamed_repos,
+        }
+
+    def get_repos(self):
+        return self.repos
+
+    def get_repos_dict(self):
+        return {repo[REPO_KEY_URL]: repo for repo in self.repos}
+
+    def get_error_repos(self):
+        return self.error_repos
+
+    def get_renamed_repos(self):
+        key_url = self.key_renamed_url
+        key_alias = self.key_renamed_alias
+        return {
+            li: items[key_url]
+            for items in self.renamed_repos.values()
+            for li in items[key_alias]
+            if items[key_url] != li
+        }
+
+    @property
+    def now(self):
+        return self._now
+
+    def save(self):
+        stats_file = self.filename
+        out = self.output()
+        logging.info(f"Save to {stats_file}")
+        with open(stats_file, "w") as f:
+            json.dump(out, f, indent=2, ensure_ascii=False)
