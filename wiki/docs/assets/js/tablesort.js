da5f9e4f9082f11316ef5235b1f55cf95e6da4b2
hantang <34468720+hantang@users.noreply.github.com>
2024-06-13 19:49:07 +0800

    update workflow

--- wiki/docs/assets/js/tablesort.js
+++
@@ -1,7 +0,0 @@
-// document$.subscribe(function() {
-document.addEventListener('DOMContentLoaded', function () {
-  var tables = document.querySelectorAll("article table:not([class])")
-  tables.forEach(function(table) {
-    new Tablesort(table)
-  })
-})
